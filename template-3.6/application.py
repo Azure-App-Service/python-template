1b641cdc9a64306614f63d6b4cdbccc097718a6d
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-10-14 17:22:25 -0700

    add check for dir exist

--- template-3.6/application.py
+++ template-3.6/application.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='/opt/defaultsite')
 
 @app.route('/')
 def root():
-    if len(next(os.walk('/home/site/deployments'))[1]) > 1:
+    if os.path.isdir('/home/site/deployments') and len(next(os.walk('/home/site/deployments'))[1]) > 1:
         return app.send_static_file('hostingstart_dep.html')
     else:
         return app.send_static_file('hostingstart.html')
